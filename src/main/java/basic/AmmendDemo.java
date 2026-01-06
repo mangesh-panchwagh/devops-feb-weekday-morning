5b3f4af5177bd5c47f2302c07ca574d7a72bfff4
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2026-01-06 14:38:16 +0530

    added ammend demo class file

---
+++ src/main/java/basic/AmmendDemo.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class AmmendDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello there !!!");
+		System.out.println("Welcome to my team!!!");
+	}
+}
